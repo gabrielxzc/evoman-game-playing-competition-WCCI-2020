23f6e4beb7a00945173fc10c04811becbf374190
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2020-05-26 00:21:13 +0300

    sync 25-5

--- scripts/pso_bootstrap.sh
+++ scripts/pso_bootstrap.sh
@@ -1,6 +1,6 @@
 source ../venv/bin/activate
 
-NR_OF_EXPERIMENTS_TO_RUN=30
+NR_OF_EXPERIMENTS_TO_RUN=8
 
 printf "Please run this script from the root folder of this repository\n"
 printf "e.g. ~/evoman-game-playing-competition$ scripts/pso_bootstrap.sh"
