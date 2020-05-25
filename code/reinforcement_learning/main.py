f2b27f2f9b228d8837ecb905a8334d46049ef3f3
Gabriel-Codrin Cojocaru <codrin.cojocaru@structuralmanagement.ro>
2020-05-25 23:35:54 +0300

    reinforcement learning parameters controlled from folder

--- code/reinforcement_learning/main.py
+++ code/reinforcement_learning/main.py
@@ -8,10 +8,22 @@ if not os.getcwd().endswith('evoman_framework'):
 
 # Start of code without hacks
 
-from spinup.utils.mpi_tools import mpi_fork
+import reinforcement_learning.parameters as parameters
+
 from spinup import ppo_pytorch as ppo
+from spinup.utils.mpi_tools import mpi_fork
 
-mpi_fork(6)
+import time
 
-logger_kwargs = dict(output_dir='path/to/output_dir', exp_name='experiment_name')
-ppo(steps_per_epoch=10000, epochs=500, logger_kwargs=logger_kwargs)
+mpi_fork(parameters.NR_PARALLEL_PROCESSES)
+
+logger_kwargs = dict(output_dir=f'../trained_models/reinforcement_learning/{time.time()}',
+                     exp_name='evoman reinforcement learning')
+seed = int(time.time())
+
+ppo(ac_kwargs={'hidden_sizes': parameters.MODEL_HIDDEN_LAYERS_SIZES, 'activation': parameters.MODEL_ACTIVATION},
+    seed=seed, steps_per_epoch=parameters.STEPS_PER_EPOCH, epochs=parameters.EPOCHS,
+    enemies=parameters.ENEMIES_CHOSEN_FOR_TRAINING, enemy_difficulty=parameters.ENEMIES_DIFFICULTY_LEVEL,
+    gamma=parameters.GAMMA, clip_ratio=parameters.CLIP_RATIO, pi_lr=parameters.PI_LR, vf_lr=parameters.VF_LR,
+    train_pi_iters=parameters.TRAIN_PI_ITERATIONS, train_v_iters=parameters.TRAIN_V_ITERATIONS, lam=parameters.LAMBDA,
+    target_kl=parameters.TARGET_KL, logger_kwargs=logger_kwargs, save_freq=parameters.SAVE_FREQ)
--- spinningup/spinup/algos/pytorch/ppo/ppo.py
+++ spinningup/spinup/algos/pytorch/ppo/ppo.py
@@ -90,7 +90,7 @@ class PPOBuffer:
 
 def ppo(actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0, steps_per_epoch=4000, epochs=50,
         enemies=[1, 4, 6, 7], enemy_difficulty=5, gamma=0.99, clip_ratio=0.2, pi_lr=3e-4, vf_lr=1e-3, train_pi_iters=80,
-        train_v_iters=80, lam=0.97, max_ep_len=1000, target_kl=0.01, logger_kwargs=dict(), save_freq=10):
+        train_v_iters=80, lam=0.97, target_kl=0.01, logger_kwargs=dict(), save_freq=10):
     """
     Proximal Policy Optimization (by clipping), 
 
