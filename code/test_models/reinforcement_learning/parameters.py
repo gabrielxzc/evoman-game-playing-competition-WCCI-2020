d60652baa5964a26fac8dba36a40998986e5bd92
Dinu Sergiu-Andrei <dinusergiuandrei@yahoo.com>
2020-05-30 01:04:18 +0300

    Trained on other 4 bots.

--- code/test_models/reinforcement_learning/parameters.py
+++ code/test_models/reinforcement_learning/parameters.py
@@ -1,4 +1,6 @@
-ENEMIES_CHOSEN_FOR_TESTING = range(1, 9)
-NR_EXPERIMENTS_FOR_EACH_ENEMY = 1
+import os
 
-MODEL_PATH = '../trained_models/reinforcement_learning/1590649982.3414495'
+ENEMIES_CHOSEN_FOR_TESTING = range(1, 9)
+NR_EXPERIMENTS_FOR_EACH_ENEMY = 30
+model_name = sorted(os.listdir('../trained_models/reinforcement_learning'))[-1]
+MODEL_PATH = f'../trained_models/reinforcement_learning/{model_name}'
